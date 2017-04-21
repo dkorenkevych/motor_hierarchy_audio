71a348af93d3da6ba06ec4565f8226f6b9414a58
dkorenkevych <dmytro.korenkevych@kindred.ai>
2017-04-21 13:31:01 -0700

    no hierarchy

--- motor_hierarchy_toy.py
+++ motor_hierarchy_toy.py
@@ -11,28 +11,32 @@ import matplotlib
 matplotlib.use('TkAgg')
 from Tkinter import *
 import numpy as np
-
-
+import time
+from threading import Thread
 
 
 class DB_Gui:
     def __init__(self, root, temporal_seq_length, encoded_dim):
         self.temporal_seq_length = temporal_seq_length
         self.encoded_dim = encoded_dim
+        self.sequence_so_far2 = [0]
+        self.sequence_so_far1 = [0]
         self.master = Frame(root)
         self.initialize()
         self.root = root
+
 
     def initialize(self):
         self.master.pack()
         left_side = Frame(self.master)
         left_side.grid(row=0, column=0, sticky=S+N+W+E)
         self.scales = []
-        for i in range(4):
+        for i in range(2):
             w = Scale(left_side, from_=0, to=100, resolution=-1, command=self.updateValue)
             w.pack()
-            w.set(50)
+            w.set(0)
             self.scales.append(w)
+        self.scales[0].set(100)
         right_side = Frame(self.master)
         right_side.grid(row=0, column=1, sticky=S + N + W + E)
         self.f = Figure(figsize=(5, 4), dpi=100)
@@ -43,52 +47,83 @@ class DB_Gui:
         toolbar.update()
         tool_frame.grid(column=0, row=1, sticky=W)
         self.create_model()
-        self.update_plot()
+        #self.drawing_thread = Thread(target=self.draw_curve)
+        #self.drawing_thread.start()
+        self.draw_curve()
+        #self.update_plot()
 
     def create_model(self):
 
-        input = Input((self.temporal_seq_length, 2))
-        decoder_input = Input((self.encoded_dim,))
-        layer = TimeDistributed(Dense(units=500, activation='relu'))(input)
-        layer = LSTM(output_dim=self.encoded_dim, return_sequences=False, activation='tanh',
-                     activity_regularizer=rgl.l1(5e-4))(layer)
-        shared_layer1 = LSTM(output_dim=500, return_sequences=True, activation='tanh', bias_regularizer=rgl.l2(1e-5),
-                             kernel_regularizer=rgl.l2(1e-5), recurrent_regularizer=rgl.l2(1e-5))
-        shared_layer2 = TimeDistributed(Dense(units=2, activation='linear', bias_regularizer=rgl.l2(1e-5),
-                                              kernel_regularizer=rgl.l2(1e-5)))
-        encoded = layer
-        representation = RepeatVector(self.temporal_seq_length)(layer)
-        representation = shared_layer1(representation)
-        output = shared_layer2(representation)
-        model = Model(input=[input], output=[output])
-        decoder = RepeatVector(self.temporal_seq_length)(decoder_input)
-        decoder = shared_layer1(decoder)
+        weight_reg = 1e-7
+        temporal_seq_length1 = 100
+        temporal_seq_length2 = 8
+        temporal_seq_length3 = 10
+        temp_data_length = 100
+        input = Input((temp_data_length, 2))
+        decoder_input = Input(batch_shape=(1, 1, encoded_dim))
+
+
+        shared_layer1 = LSTM(output_dim=50, return_sequences=True, stateful=True, activation='tanh',
+                             bias_regularizer=rgl.l2(weight_reg),
+                             kernel_regularizer=rgl.l2(weight_reg), recurrent_regularizer=rgl.l2(weight_reg))
+
+        shared_layer2 = TimeDistributed(Dense(output_dim=2, activation='linear', bias_regularizer=rgl.l2(weight_reg),
+                                              kernel_regularizer=rgl.l2(weight_reg)))
+
+        shared_layer3 = LSTM(output_dim=100, return_sequences=True, stateful=True, activation='tanh',
+                             bias_regularizer=rgl.l2(weight_reg),
+                             kernel_regularizer=rgl.l2(weight_reg), recurrent_regularizer=rgl.l2(weight_reg))
+
+        shared_layer4 = LSTM(output_dim=100, return_sequences=True, activation='tanh', bias_regularizer=rgl.l2(weight_reg),
+                             kernel_regularizer=rgl.l2(weight_reg), recurrent_regularizer=rgl.l2(weight_reg))
+
+
+
+        #decoder = RepeatVector(temporal_seq_length1)(decoder_input)
+        decoder = shared_layer1(decoder_input)
+        # decoder = TimeDistributed(RepeatVector(temporal_seq_length2))(decoder)
+        # decoder = Reshape((temporal_seq_length2, 500))(decoder)
+        decoder = shared_layer3(decoder)
+        # decoder = TimeDistributed(TimeDistributed(RepeatVector(temporal_seq_length3)))(decoder)
+        # decoder = shared_layer4(decoder)
+        # decoder = Reshape((temporal_seq_length1 * temporal_seq_length2 * temporal_seq_length3, 500))(decoder)
         decoder = shared_layer2(decoder)
-        decoder_model = Model(input=[decoder_input], output=decoder)
-        encoder_model = Model(input=[input], output=[encoded])
-        model.compile(optimizer=Adam(decay=1e-5), loss='mse')
-        self.model = model
-        self.encoder = encoder_model
-        self.decoder = decoder_model
+        self.decoder = Model(input=[decoder_input], output=decoder)
+
+
+
         self.decoder.load_weights("decoder.json")
 
     def update_plot(self):
-        data = np.zeros((1, 4))
-        for i in range(4):
+        data = np.zeros((1, 2))
+        for i in range(2):
             data[0, i] = self.scales[i].get()/50.0 - 1
         #print data
-        output = self.decoder.predict(data)
-        self.f.clear()
-        self.a = self.f.add_subplot(111)
-        self.a.plot(output[0, :, 0])
-        self.plot_area.show()
+        output = self.decoder.predict(data.reshape(1, 1, 2))
+        self.sequence_so_far1.append(output[0, 0, 0])
+        self.sequence_so_far2.append(output[0, 0, 1])
+        # self.f.clear()
+        # self.a = self.f.add_subplot(111)
+        # self.a.plot(output[0, :, 0])
+        # self.plot_area.show()
 
     def updateValue(self, event):
         self.update_plot()
 
+    def draw_curve(self):
+        self.update_plot()
+        self.f.clear()
+        self.a = self.f.add_subplot(111)
+        self.a.plot(self.sequence_so_far1)
+        self.a.plot(self.sequence_so_far2)
+        self.plot_area.show()
+        #time.sleep(0.2)
+        root.after(200, lambda: self.draw_curve())
+
+
 if __name__ == "__main__":
     temporal_seq_length = 50
-    encoded_dim = 4
+    encoded_dim = 2
     root = Tk()
     root.title('GYM results database')
     gui = DB_Gui(root, temporal_seq_length, encoded_dim)
